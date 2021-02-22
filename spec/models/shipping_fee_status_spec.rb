0324077a0908db1ece94b9ff93a3bc2c7b47f1bf
shorimpo <7se972583z79x@gmail.com>
2021-02-22 12:45:21 +0900

    商品一覧表示機能

--- spec/models/shipping_fee_status_spec.rb
+++ spec/models/shipping_fee_status_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe Shippingfee, type: :model do
+RSpec.describe ShippingfeeStatus, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
 end
