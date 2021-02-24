0b5873780be3d0f45e0f387d8c6b2d5a0771bda9
shorimpo <7se972583z79x@gmail.com>
2021-02-24 17:09:08 +0900

    商品削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :redirect_motion, only: [:edit, :update]
   def index
     @items = Item.all.order('created_at DESC')
@@ -31,6 +31,12 @@ class ItemsController < ApplicationController
     else
       render action: :edit
     end
+  end
+
+  def destroy
+    if @item.destroy
+      redirect_to root_path
+    end 
   end
 
   private
