72ed385628c9a0ba4191c3635d00289dc449d6c4
shorimpo <7se972583z79x@gmail.com>
2021-02-24 16:29:03 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
+
   def index
     @items = Item.all.order('created_at DESC')
   end
@@ -13,6 +14,13 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+    unless current_user.id == @item.user_id
+      redirect_to root_path
+    end
+  end
+
   def create
     @item = Item.new(item_params)
     if @item.save
@@ -22,10 +30,22 @@ class ItemsController < ApplicationController
     end
   end
 
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render action: :edit
+    end
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:name, :info, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :price,
                                  :scheduled_delivery_id, :image).merge(user_id: current_user.id)
   end
+
+ 
 end
